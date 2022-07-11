25ac8d7df656b0c3eaf4a39bc13512f418b59827
Alex Wu <yizhouwu99@gmail.com>
2022-07-11 11:44:40 -0400

    change to elevatorControl.php

--- PHP/elevatorControl.php
+++ PHP/elevatorControl.php
@@ -67,14 +67,14 @@
 		<?php 
 			if(isset($_POST['newfloor'])) {
 				$curFlr = update_elevatorNetwork(1, $_POST['newfloor']); 
-				header('Refresh:0; url=index.php');	
+				header('Refresh:0; url=member.php');	
 			} 
 			$curFlr = get_currentFloor();
 			echo "<h2>Current floor # $curFlr </h2>";			
 		?>		
 		
 		<h2> 	
-			<form action="elevatorControl.php" method="POST">
+			<form action="member.php" method="POST">
 				Request floor # <input type="number" style="width:50px; height:40px" name="newfloor" max=3 min=1 required />
 				<input type="submit" value="Go"/>
 			</form>
