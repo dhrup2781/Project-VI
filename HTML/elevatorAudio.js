d302d8e1000f27714cc968f4e3f7c023115252dd
unknown <dhrupnpatel2781@gmail.com>
2022-07-11 12:47:01 -0400

    added audio

--- HTML/elevatorAudio.js
+++ HTML/elevatorAudio.js
@@ -3,3 +3,21 @@ function playMusic(){
     music.play();
     music.currentTime=0;
     }
+
+function playMusic3(){
+    var music = new Audio('../audio/floor3.mp3');
+    music.play();
+    music.currentTime=0;
+    }
+
+function playMusic2(){
+    var music = new Audio('../audio/floor2.mp3');
+    music.play();
+    music.currentTime=0;
+    }
+
+function playMusic1(){
+    var music = new Audio('../audio/floor1.mp3');
+    music.play();
+    music.currentTime=0;
+    }
