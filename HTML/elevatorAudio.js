16f619e5c2ec8cdc1b033c8f6970c3a47b087ef0
dhrup2781 <dhrupnpatel2781@gmail.com>
2022-07-13 16:38:33 -0400

    elevatorControl.php is updated with audio

--- HTML/elevatorAudio.js
+++ HTML/elevatorAudio.js
@@ -1,31 +1,29 @@
-var delay = 5000;
-
-setTimeout (function playMusicDO(){
+function playMusicDO(){
     var music = new Audio('../audio/dooropen.mp3');
     music.play();
     music.currentTime=0;
-    }, delay );
+    }
 
-setTimeout (function playMusicDC(){
+function playMusicDC(){
     var music = new Audio('../audio/doorclose.mp3');
     music.play();
     music.currentTime=0;
-    }, delay);
+    }
 
-setTimeout (function playMusic3(){
+function playMusic3(){
     var music = new Audio('../audio/floor3.mp3');
     music.play();
     music.currentTime=0;
-    }, delay);
+    }
 
-setTimeout (function playMusic2(){
+function playMusic2(){
     var music = new Audio('../audio/floor2.mp3');
     music.play();
     music.currentTime=0;
-    }, delay);
+    }
 
-setTimeout (function playMusic1(){
+function playMusic1(){
     var music = new Audio('../audio/floor1.mp3');
     music.play();
     music.currentTime=0;
-    }, delay);
+    }
