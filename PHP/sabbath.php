fd87be0b50ce3e167baf8b2f04c9ac5afd966355
Alex Wu <yizhouwu99@gmail.com>
2022-07-14 11:36:47 -0400

    minor change to sabbath

--- PHP/sabbath.php
+++ PHP/sabbath.php
@@ -55,9 +55,9 @@
         // get the q parameter from URL
         $q = $_REQUEST["q"];        // In this case $q == "" empty string
 
-        $currfloor = strval(rand(1,20));   // This is what gets sent back to the 'q=' AJAX request
-        // $currfloor = strval(read_currentFloor($path, $user, $password, $tablename));
-
+        // $currfloor = strval(rand(1,20));   // This is what gets sent back to the 'q=' AJAX request
+        $currfloorRead = read_currentFloor($path, $user, $password, $tablename);
+        $currfloor = strval($currfloorRead);
         // $currfloor = 1;
 
         // string sent to function that handles the 'onreadystatechange' event
