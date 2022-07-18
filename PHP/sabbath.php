c7c39a79f5697b0b77de7edb5c6a90da034c1204
dhrup2781 <dhrupnpatel2781@gmail.com>
2022-07-18 13:54:02 -0400

    sabbath mode updated

--- PHP/sabbath.php
+++ PHP/sabbath.php
@@ -26,15 +26,6 @@
         }
         return $presentFloor;
     }
-    function show_currentFloor(string $path, string $user, string $pass, $tablename) {
-        $db = connect($path, $user, $pass); 
-        $query = "SELECT * FROM $tablename";  // Note: Risk of SQL injection
-        $rows = $db->query($query); 
-        echo "Current floor: ";
-        foreach ($rows as $row) {
-            echo $row['currentFloor'] . "<br>";
-        }
-    }
 
     // Update
     function update_currentFloor(string $path, string $user, string $password, string $tablename, int $new_currentFloor ) : void {
@@ -45,7 +36,7 @@
         $statement->execute();                      // Execute prepared statement
 }
 
-        //header("Refresh:10");
+        header("Refresh:10");
     // if(isset($_SESSION['username'])) {
         // Initialize variables
         $host = '127.0.0.1'; 
@@ -53,140 +44,65 @@
         $tablename = 'elevatorNetwork'; 
         $path = 'mysql:host=' . $host . ';dbname=' . $database; 
         $user = 'ese';  // Could be a variable from $_SESSION['username'] if the database has been set up with permissions for another user
-        $pass = 'ese';
+        $password = 'ese';
         // $node_ID = 1;
 
 
         // Connect to database and make changes
-        $db = connect($path, $user, $pass);
+        $db = connect($path, $user, $password);
         
-        //showtable($path, $user, $password, $tablename);
+        showtable($path, $user, $password, $tablename);
 
 
         // get the q parameter from URL
-        //$q = $_REQUEST["q"];        // In this case $q == "" empty string
+        $q = $_REQUEST["q"];        // In this case $q == "" empty string
 
-        if(isset($_POST['sabbath'])) {
-            $flag = 1;
-            // $currfloor = strval(rand(1,20));   // This is what gets sent back to the 'q=' AJAX request
-            //$currfloor = read_currentFloor($path, $user, $pass, $tablename);
-            // $currfloor = strval($currfloorRead);
-            // $currfloor = 1;
+        // $currfloor = strval(rand(1,20));   // This is what gets sent back to the 'q=' AJAX request
+        $currfloor = read_currentFloor($path, $user, $password, $tablename);
+        // $currfloor = strval($currfloorRead);
+        // $currfloor = 1;
 
-            // string sent to function that handles the 'onreadystatechange' event
+        // string sent to function that handles the 'onreadystatechange' event
+        // echo "Current Floor: " . json_encode($currfloor);
+    // while(1) {
+        if ($currfloor == 1) {
             // echo "Current Floor: " . json_encode($currfloor);
-            while($flag == 1) {
-                $currfloor = read_currentFloor($path, $user, $pass, $tablename);
-                if ($currfloor == 1) {
-                    // echo "Current Floor: " . json_encode($currfloor);
-                    $newfloor = 2;
-                    update_currentFloor($path, $user, $pass, $tablename, $newfloor );
-                    // sleep(9);
-                    
-                }
-                else if ($currfloor == 2) {
-                    // echo "Current Floor: " . json_encode($currfloor);
-                    $newfloor = 3;
-                    update_currentFloor($path, $user, $pass, $tablename, $newfloor );
-                    // sleep(9);
-                }
-                else if ($currfloor == 3) {
-                    // echo "Current Floor: " . json_encode($currfloor);
-                    $newfloor = 1;
-                    update_currentFloor($path, $user, $pass, $tablename, $newfloor );
-                    // sleep(9);
-                } 
-                else if ($currfloor == 0) {
-                    // echo "Current Floor: " . json_encode($currfloor);
-                    $newfloor = 1;
-                    update_currentFloor($path, $user, $pass, $tablename, $newfloor );
-                    // sleep(9);
-                } 
-                else if(isset($_POST['stop'])) { 
-
-                    $flag = 0;
-
-                    //update_currentFloor($path, $user, $pass, $tablename, "0" ); 
-                }
-            }
-        } 
-        show_currentFloor( $path,  $user,  $pass, "elevatorNetwork");
+            $newfloor = 2;
+            update_currentFloor($path, $user, $password, $tablename, $newfloor );
+            // sleep(9);
+            
+        }
+        else if ($currfloor == 2) {
+            // echo "Current Floor: " . json_encode($currfloor);
+            $newfloor = 3;
+            update_currentFloor($path, $user, $password, $tablename, $newfloor );
+            // sleep(9);
+        }
+        else if ($currfloor == 3) {
+            // echo "Current Floor: " . json_encode($currfloor);
+            $newfloor = 1;
+            update_currentFloor($path, $user, $password, $tablename, $newfloor );
+            // sleep(9);
+        }
     // }
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-	<head> 	
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>sabbath mode</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />  
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>      			
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>   			
-		<link rel="stylesheet" type="text/css" href="../CSS/sabbath.css" /> 		
-	</head>
-    <body>
-        <style>
-            body {
-                background-image: url(../images/background.png);
-                background-repeat: no-repeat;
-                background-attachment: fixed;
-                background-size: cover;
-            }
-        </style>
-        <div id="head_wrap">
-			<header>
-				<nav class="navbar navbar-inverse navbar-fixed-top top_menu" role="navigation">	
-					<!-- Logo and collapsible menu --> 
-					<div class="navbar-header">
-						<!-- Collapsible version of navbar goes here with a target to the id below -->
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a href="index.html" class="navbar-brand">Project VI</a>
-					</div>
-					<!-- Side Menu - collapses to small menu on small screens -->
-					<div class="collapse navbar-collapse" id="myNavbar">
-						<ul class="nav navbar-nav navbar-right s_menu">
-							<li><a href="../HTML/about.html">About</a></li>
-							<li><a href="../HTML/project_plan.html">Plan</a></li>
-							<li><a href="../HTML/logbook.html">Log Book</a></li>
-							<li><a href="../HTML/login.html">Log in</a></li>
-							<li><a href="../HTML/request_access.html">Signup</a></li>
-							<li><a href="../PHP/sabbath.php">Sabbath mode</a></li> 
-							<li><a href="../HTML/resources.html">Resources</a></li>
-						</ul>
-					</div>
-				</nav>	
-			</header>
-		</div>
-        <br/>
-		<br/>
-		<br/>
-		<div id="page_wrap">
-			<form action="sabbath.php" method="post">
-				<div id="heading">
-					<h1>Sabbath mode</h1><br/><br/>
-				</div>
-				<div id="elevatorButtons">
-					<div id="extraButtons">
-						<button type="submit" class="btn btn-secondary btn-lg"  id="sabbath" name="sabbath">SABBATH</button><br/><br/> 
-					</div>
-					<div id="stopButton">
-						<button type="submit" class="btn btn-danger s btn-lg"  id="stop" name="stop">STOP</button><br/><br/><br/>
-					</div>
-				</div>
-			</form>
-		</div>
-        <br/>
-        <br/>
-		<script src="elevatorAudio.js"></script>
-		<footer>
-			<p>Copyright &copy Dhrup Patel; Alex Wu; Mike Wu</p>
-		</footer>
-    </body>
+<html>
+<head> 
+</head>
+
+<body>
+  <p> Current floor number - updates every 5 seconds or after pressing button </p>
+  <form action="">
+    <button id="myButton">Get current floor now!</button>  
+  </form>
+  <p> Current Floor: <span id='floor'></span> </p>
+
+  <!-- Add JavaScript before closing body tag -->
+  <script src='../JS/getFloor.js'></script>
+  <!-- Initialize a global variable for the floor number-->
+  <script type='text/javascript'>var floor;</script>
+</body>
 </html>
