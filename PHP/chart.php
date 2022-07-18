ead941158131b6bddb60783bb16996e74bbf6d87
Alex Wu <yizhouwu99@gmail.com>
2022-07-18 15:23:12 -0400

    added chart.php

--- PHP/chart.php
+++ PHP/chart.php
@@ -9,7 +9,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>      			
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>   	
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.0/Chart.min.js"></script>
-  <title>Document</title>
+  <title>Floor Chart</title>
 </head>
 <body>
 
